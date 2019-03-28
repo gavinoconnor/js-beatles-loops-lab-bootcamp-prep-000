6ed5e5574e8aa4f21f37f750569089d04b0dd5c5
Gavin O'Connor <gedmund@gmail.com>
2019-03-28 17:15:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ var johnLennonFacts = function(factsArray) {
   }
   return facts
 }
-
 // + Create a function `iLoveTheBeatles` which accepts a number as a parameter. The
 // body of the function should create a variable that stores an empty array. Then,
 // implement a do-while loop inside the function that adds `"I love the Beatles!"`
@@ -32,3 +31,12 @@ var johnLennonFacts = function(factsArray) {
 // parameter. The condition of the loop should check to see that the parameter
 // number is less than `15`. The function should return the array with the strings
 // `"I love the Beatles!"`.
+var iLoveTheBeatles = function(n) {
+  let beatlesArray = []
+
+  do {
+    beatlesArray.push(`I love the Beatles!`)
+    n++
+  } while (n < 15)
+  return beatlesArray
+}
